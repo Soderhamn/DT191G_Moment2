711d06ab4f2dd825cb626fd0dc58d999ab2d3410
Marcus Andersson <marcus@apparant.se>
2023-02-01 19:45:41 +0100

    Skapat Model och fler vyer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Moment2.Models;
+using Moment2.Models;  //Inkludera Models
 using System.Diagnostics;
 
 namespace Moment2.Controllers
@@ -13,18 +13,34 @@ namespace Moment2.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() //Index-view
+        {
+            ViewData["Title"] = "Startsidan"; //Använder ViewData för att returnera titeln från Controllern
+            ViewData["Metod"] = "MVC";
+            return View();
+        }
+
+        public IActionResult MyPhone() {
+            CellphoneModel cp = new CellphoneModel(); //Skapa ny instans av model
+
+            //Ange värden på medlemsvariablerna
+            cp.Brand = "Xiaomi";
+            cp.Colour = "Svart";
+            cp.Price = 1595;
+
+            //Returnera instansen av modellen till vyn
+            return View(cp);
+        }
+
+        public IActionResult Cellphone() //Cellphone view
         {
             return View();
         }
 
-        public IActionResult Cellphone()
+        public IActionResult About() //About View
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
+            ViewBag.AuthorName = "Marcus Andersson"; //Använd Viewbag för att returnera data till View
+            ViewBag.CourseName = "DT191G";
             return View();
         }
 
--- Models/CellphoneModel.cs
+++ Models/CellphoneModel.cs
@@ -1,7 +1,9 @@
-﻿namespace Moment2.Models
+﻿using System;
+namespace Moment2.Models
 {
     public class CellphoneModel
     {
+        public int Id { get; set; }
         public string? Brand { get; set; }
         public string? Colour { get; set; }
         public double Price { get; set; }
