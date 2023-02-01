d00e4762a1c7cbf0ba45bb07daa8179426e35726
Marcus Andersson <marcus@apparant.se>
2023-02-01 20:44:30 +0100

    Sparar data i Cookies mellan sidor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,12 +15,26 @@ namespace Moment2.Controllers
 
         public IActionResult Index() //Index-view
         {
+
+            if(HttpContext.Request.Cookies.ContainsKey("PhoneName")) //Om kakan PhoneName finns
+            {
+                string? pn = HttpContext.Request.Cookies["PhoneName"]; //Läs in namnet från Cookie
+                ViewData["SavedPhone"] = pn; //Lagra i ViewData["SavedPhone"]
+            }
+
             ViewData["Title"] = "Startsidan"; //Använder ViewData för att returnera titeln från Controllern
             ViewData["Metod"] = "MVC";
             return View();
         }
 
         public IActionResult MyPhone() {
+
+            if(HttpContext.Request.Cookies.ContainsKey("PhoneName")) //Om kakan PhoneName finns
+            {
+                string? pn = HttpContext.Request.Cookies["PhoneName"]; //Läs in namnet från Cookie
+                ViewData["SavedPhone"] = pn; //Lagra i ViewData["SavedPhone"]
+            }
+
             CellphoneModel cp = new CellphoneModel(); //Skapa ny instans av model
 
             //Ange värden på medlemsvariablerna
@@ -31,14 +45,42 @@ namespace Moment2.Controllers
             //Returnera instansen av modellen till vyn
             return View(cp);
         }
+        [HttpPost] //Vid postning
+        public IActionResult Cellphone(CellphoneModel cp) {
 
+
+            ViewBag.CellphoneInfo = cp.Brand + " " + cp.Colour + " " + cp.Price + ":-";
+
+            //Spara telefonens namn i en Cookie
+            HttpContext.Response.Cookies.Append("PhoneName", cp.Brand + "");
+
+            ViewData["SavedPhone"] = cp.Brand; //Lagra i ViewData["SavedPhone"]
+            
+
+            return View();
+        }
+
+        [HttpGet] //Visa 
         public IActionResult Cellphone() //Cellphone view
         {
+            
+            if(HttpContext.Request.Cookies.ContainsKey("PhoneName")) //Om kakan PhoneName finns
+            {
+                string? pn = HttpContext.Request.Cookies["PhoneName"]; //Läs in namnet från Cookie
+                ViewData["SavedPhone"] = pn; //Lagra i ViewData["SavedPhone"]
+            }
+
             return View();
         }
 
         public IActionResult About() //About View
         {
+            if(HttpContext.Request.Cookies.ContainsKey("PhoneName")) //Om kakan PhoneName finns
+            {
+                string? pn = HttpContext.Request.Cookies["PhoneName"]; //Läs in namnet från Cookie
+                ViewData["SavedPhone"] = pn; //Lagra i ViewData["SavedPhone"]
+            }
+
             ViewBag.AuthorName = "Marcus Andersson"; //Använd Viewbag för att returnera data till View
             ViewBag.CourseName = "DT191G";
             return View();
@@ -49,5 +91,6 @@ namespace Moment2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
--- Models/CellphoneModel.cs
+++ Models/CellphoneModel.cs
@@ -3,9 +3,9 @@ namespace Moment2.Models
 {
     public class CellphoneModel
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string? Brand { get; set; }
         public string? Colour { get; set; }
-        public double Price { get; set; }
+        public double? Price { get; set; }
     }
 }
